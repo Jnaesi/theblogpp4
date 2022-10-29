499dda89646c9f7002e97df9fe8e7ea73ff12987
Jnaesi <jacobnasi@hotmail.com>
2022-10-29 12:08:26 +0000

    Add forms with bootstrap

---
+++ theblog/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'author', 'body')
+
+        widgets = {
+            'title': form.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': form.TextInput(attrs={'class': 'form-control'}),
+            'author': form.Select(attrs={'class': 'form-control'}),
+            'body': form.Textarea(attrs={'class': 'form-control'}),
+        }
+        
