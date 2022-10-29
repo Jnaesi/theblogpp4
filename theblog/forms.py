108f7286d594ff8b967d2cfcf5db17535124f365
Jnaesi <jacobnasi@hotmail.com>
2022-10-29 12:16:50 +0000

    Fixed issue misspelling on forms in widgets

--- theblog/forms.py
+++ theblog/forms.py
@@ -7,9 +7,8 @@ class PostForm(forms.ModelForm):
         fields = ('title', 'title_tag', 'author', 'body')
 
         widgets = {
-            'title': form.TextInput(attrs={'class': 'form-control'}),
-            'title_tag': form.TextInput(attrs={'class': 'form-control'}),
-            'author': form.Select(attrs={'class': 'form-control'}),
-            'body': form.Textarea(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
-        
