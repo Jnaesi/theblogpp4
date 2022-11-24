d7a5ab04c9113581efbfe295a964027c01105893
Jnaesi <jacobnasi@hotmail.com>
2022-11-24 13:04:57 +0000

    Made a few changes and added a proflie page for better navigation

--- members/views.py
+++ members/views.py
@@ -9,6 +9,13 @@ from theblog.models import Profile
 
 # Create your views here.
 
+class EditProfilePageView(generic.UpdateView):
+	model = Profile
+	template_name = 'registration/edit_profile_page.html'
+	fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url', 'pinterest_url']
+	success_url = reverse_lazy('home')
+
+
 class ShowProfilePageView(DetailView):
 	model = Profile
 	template_name = 'registration/user_profile.html'
@@ -22,12 +29,13 @@ class ShowProfilePageView(DetailView):
 		context["page_user"] = page_user
 		return context
 
+
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
-class UserEditView(generic.CreateView):
+class UserEditView(generic.UpdateView):
     form_class = EditProfileForm
     template_name = 'registration/edit_profile.html'
     success_url = reverse_lazy('home')
--- theblog/views.py
+++ theblog/views.py
@@ -9,15 +9,16 @@ from django.http import HttpResponseRedirect
 
 
 def LikeView(request, pk):
-    post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    liked = False
-    if post.likes.filter(id=request.user.id).exists():
-        post.likes.remove(request.user)
-        liked = False
-    else:
-        post.likes.add(request.user)
-        liked = True
-        return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
+	post = get_object_or_404(Post, id=request.POST.get('post_id'))
+	liked = False
+	if post.likes.filter(id=request.user.id).exists():
+		post.likes.remove(request.user)
+		liked = False
+	else:
+		post.likes.add(request.user)
+		liked = True
+	
+	return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
 
 class HomeView(ListView):
